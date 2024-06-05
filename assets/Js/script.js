506932be63a556908e15ec652c6472b0de5d9aca
MuhammadUsman <imdevusman@gmail.com>
2024-06-05 16:21:12 +0500

    main

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $("#image-carousel").owlCarousel({
-        items: 4,
+        items: 3,
         navigation: true,
         pagination: true,
         autoplay: true,
@@ -11,7 +11,26 @@ $(document).ready(function() {
 });
 
 $('#image-carousel .item img').on('click', function(){
-    
     var path = $(this).attr("src");
     $('#image-container').html('<img src="' + path + '" alt="Selected Image">');
 });
+
+
+
+// $('.btn').on('click', function(){
+//     $('#image-container').html('<img src="' +  + '" alt="Selected Image">');
+// });
+
+
+// document.querySelectorAll('.btn').forEach(function(btn) {
+//     btn.addEventListener('click', function() {
+//         document.getElementById('image-container').innerHTML = '<img src="' +  + '" alt="Selected Image">';
+//     });
+// });
+
+
+const Btn = document.getElementsByClassName('btn');
+const ImageContainer = document.getElementById('image-container')
+Btn[0].addEventListener('click', () => {
+    ImageContainer.innerHTML = '<img src="' +  + '" alt="Selected Image">'
+})
